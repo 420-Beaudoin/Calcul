c504c8a72cd58a2fca9ce763516d12d6a7ba45bc
420-Beaudoin <france.beaudoin@cegep-lanaudiere.qc.ca>
2021-02-01 17:43:56 -0500

    ajout méthode division

--- Calcul.java
+++ Calcul.java
@@ -14,4 +14,11 @@ public class Calcul {
     public static int multiplication(int n1, int n2){
         return n1 * n2;
     }
+
+    public static double division(int n1, int n2){
+        if (n2 != 0)
+            return n1 / n2;
+        else
+            return 0;
+    }
 }
